7a6baee703dc74f04eda8faf52dd8c13363972ae
16-sanalmishra <sanal.m1600@gmail.com>
2020-08-23 15:27:47 +0530

    Onclick Event added for login button.

--- app/src/main/java/com/example/first_project/MainActivity.java
+++ app/src/main/java/com/example/first_project/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.first_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    public void clickbtn(View view){
+        Log.i("Loginbtn", "clickbtn: The button is clicked ");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
